42aca0ef12b4303b0b3f71ebb5a862274c6fd050
Liu Lixiang <liulixiang1988@gmail.com>
2013-03-04 17:32:09 +0800

    asp.net mvc3 charpter2

---
+++ MvcMusicStore/MvcMusicStore/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "我喜欢吃水果!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public string Index()
+        {
+            return "Hello from StoreController.Index()";
+        }
+
+        //
+        // Get:/Store/Browse?genre=?Disco
+        public string Browse(string genre)
+        {
+            string message = HttpUtility.HtmlEncode("Hello from StoreController.Browse()" + genre);
+            return message;
+        }
+
+        //
+        // Get:/Store/Details/5
+        public string Details(string id)
+        {
+            string message = "Store.Details, ID = " + id;
+            return message;
+        }
+
+    }
+}
