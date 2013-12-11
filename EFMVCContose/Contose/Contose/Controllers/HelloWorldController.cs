0c47c8b5eab0c13ba993fff0acdfc42d087587c1
liulixiang1988 <liulixiang1988@gmail.com>
2013-12-11 23:39:07 +0800

    控制器参数传递

---
+++ EFMVCContose/Contose/Contose/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Contose.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("你好，" + name + ", NumTimes:" + numTimes);
+        }
+	}
+}
