3e63bf9405a7646fb4e65ee7243a5028cb3597fb
liulixiang1988 <liulixiang1988@gmail.com>
2014-05-09 16:22:41 +0800

    FReportCoil

---
+++ FReportCoil/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FReportCoil
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FmMain("1210412740-0-0-B-22"));
+        }
+    }
+}
