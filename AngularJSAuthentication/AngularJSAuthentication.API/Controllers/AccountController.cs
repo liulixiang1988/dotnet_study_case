db2ac44aad7b4aff9fb4f750df739dc2760f3a20
liulixiang1988 <liulixiang1988@gmail.com>
2014-10-30 17:17:13 +0800

    add AccountController

---
+++ AngularJSAuthentication/AngularJSAuthentication.API/Controllers/AccountController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using AngularJSAuthentication.API.Models;
+using Microsoft.AspNet.Identity;
+
+namespace AngularJSAuthentication.API.Controllers
+{
+    [RoutePrefix("/api/account")]
+    public class AccountController : ApiController
+    {
+        private AuthRepository _repo = null;
+
+        public AccountController()
+        {
+            _repo = new AuthRepository();
+        }
+
+        //POST /api/Acount/Register
+        [AllowAnonymous]
+        [Route("Register")]
+        public async Task<IHttpActionResult> Register(UserModel userModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            IdentityResult result = await _repo.RegisterUser(userModel);
+            IHttpActionResult errorResult = GetErrorResult(result);
+            if (errorResult != null)
+            {
+                return errorResult;
+            }
+
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _repo.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private IHttpActionResult GetErrorResult(IdentityResult result)
+        {
+            if (result == null)
+            {
+                return InternalServerError();
+            }
+
+            if (!result.Succeeded)
+            {
+                if (result.Errors != null)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+
+                if (ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+                return BadRequest(ModelState);
+            }
+            return null;
+        }
+    }
+}
