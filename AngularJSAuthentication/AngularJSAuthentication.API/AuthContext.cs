95de0f9f70f2bc971d4f54a20b2eb2b4d45b5555
liulixiang1988 <liulixiang1988@gmail.com>
2014-10-30 16:20:33 +0800

    add AuthContext

---
+++ AngularJSAuthentication/AngularJSAuthentication.API/AuthContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace AngularJSAuthentication.API
+{
+    public class AuthContext: IdentityDbContext<IdentityUser>
+    {
+        public AuthContext() : base("AuthContext")
+        {
+            
+        }
+    }
+}
