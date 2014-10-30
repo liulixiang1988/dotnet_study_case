2eab9d8f4083b0c2175e296908680202d9fc9c54
liulixiang1988 <liulixiang1988@gmail.com>
2014-10-30 15:37:30 +0800

    Web API认证 - 01 创建应用

---
+++ AngularJSAuthentication/AngularJSAuthentication.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularJSAuthentication.API.WebApiApplication" Language="C#" %>
---
+++ AngularJSAuthentication/AngularJSAuthentication.API/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+using AngularJSAuthentication.API;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof (Startup))]
+
+namespace AngularJSAuthentication.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
