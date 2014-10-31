fb7dcb47f7a951f8c48c13bfa2cfd96612f4e047
liulixiang1988 <liulixiang1988@gmail.com>
2014-10-31 23:31:24 +0800

    display and validate model

--- AspNetMVCEssential/AspNetMVCEssential/Models/CheckoutAccount.cs
+++ AspNetMVCEssential/AspNetMVCEssential/Models/CheckoutAccount.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,12 +10,22 @@ namespace AspNetMVCEssential.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        //[StringLength(10, MinimumLength = 6)]
+        [RegularExpression(@"\d{6,10}", ErrorMessage = "账户必须是6-10个字符")]
+        [Display(Name = "账户")]
         public string AccountNumber { get; set; }
 
+        [Required]
         public string FirstName { get; set; }
 
+        [Required]
         public string LastName { get; set; }
+        [Display(Name = "姓名")]
+        public string Name { get { return FirstName + " " + LastName; } }
 
+        [Required]
+        [DataType(DataType.Currency)]
         public int Balance { get; set; }
     }
 }
