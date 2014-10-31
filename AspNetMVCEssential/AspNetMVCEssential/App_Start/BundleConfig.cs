aabb0e4f0c5ba1c8be4d55c59edca78455756ed5
liulixiang1988 <liulixiang1988@gmail.com>
2014-10-31 16:27:44 +0800

    modify Bundle

--- AspNetMVCEssential/AspNetMVCEssential/App_Start/BundleConfig.cs
+++ AspNetMVCEssential/AspNetMVCEssential/App_Start/BundleConfig.cs
@@ -26,6 +26,8 @@ namespace AspNetMVCEssential
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
