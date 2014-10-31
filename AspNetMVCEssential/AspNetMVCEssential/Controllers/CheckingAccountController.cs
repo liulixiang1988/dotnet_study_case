4559ee6f3fcd84405321095bac06e62c449ec0ac
liulixiang1988 <liulixiang1988@gmail.com>
2014-10-31 21:30:40 +0800

    add details view

---
+++ AspNetMVCEssential/AspNetMVCEssential/Controllers/CheckingAccountController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AspNetMVCEssential.Models;
+
+namespace AspNetMVCEssential.Controllers
+{
+    public class CheckingAccountController : Controller
+    {
+        //
+        // GET: /CheckingAccount/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /CheckingAccount/Details/
+        public ActionResult Details()
+        {
+            var checkingAccount = new CheckoutAccount
+            {
+                AccountNumber = "00000123456",
+                FirstName = "刘",
+                LastName = "理想",
+                Balance = 500
+            };
+            return View(checkingAccount);
+        }
+
+        //
+        // GET: /CheckingAccount/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /CheckingAccount/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /CheckingAccount/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /CheckingAccount/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /CheckingAccount/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /CheckingAccount/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AspNetMVCEssential/AspNetMVCEssential/Models/CheckoutAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMVCEssential.Models
+{
+    public class CheckoutAccount
+    {
+        public int Id { get; set; }
+
+        public string AccountNumber { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Balance { get; set; }
+    }
+}
