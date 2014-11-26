dffbc2c12abfa8fd13985915aa830cb6fc0c6353
liulixiang1988 <liulixiang1988@gmail.com>
2014-11-26 11:31:24 +0800

    Mysql Entity Framework Intial

---
+++ MySqlEFDemo/MySqlEFDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MySqlEFDemo.Startup))]
+namespace MySqlEFDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
