99fafd95b89a7f799249d534e305067f0e25a059
liulixiang1988 <liulixiang1988@gmail.com>
2014-12-04 22:48:10 +0800

    添加测试

--- AspNetMVCEssential/AspNetMVCEssential.Tests/Properties/AssemblyInfo.cs
+++ AspNetMVCEssential/AspNetMVCEssential.Tests/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// 有关程序集的常规信息是通过以下项进行控制的
+// 有关程序集的常规信息通过以下特性集 
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("AspNetMVCEssential.Tests")]
@@ -10,26 +10,27 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("AspNetMVCEssential.Tests")]
-[assembly: AssemblyCopyright("版权所有(C) Microsoft 2014")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// 将 ComVisible 设置为 false 将使此程序集中的类型
-// 对 COM 组件不可见。如果需要
-// 从 COM 访问此程序集中的某个类型，请针对该类型将 ComVisible 特性设置为 true。
+// 将 ComVisible 设置为 false 会使此程序集中的类型 
+// 对 COM 组件不可见。  如果需要从 COM 访问此程序集中的类型，
+// 请将该类型上的 ComVisible 特性设置为 true。
 [assembly: ComVisible(false)]
 
-// 如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
-[assembly: Guid("b3ae4846-651a-48d4-ae64-b1be397151c3")]
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+[assembly: Guid("9d49efe4-8249-4721-a1cc-8545105b8fa0")]
 
-// 程序集的版本信息由下列四个值组成:
+// 程序集的版本信息由以下四个值组成: 
 //
 //      主版本
 //      次版本
-//      内部版本号
-//      修订版本
+//      生成号
+//      修订号
 //
-// 你可以指定所有值，也可以让修订版本和内部版本号采用默认值，
-// 方法是按如下所示使用 "*":
+// 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
+// 方法是按如下所示使用“*”: 
+// [assembly:  AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
