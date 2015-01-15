8fc9d94eca4b57090b60d6a906e5d2e57f7e61a4
liulixiang1988 <liulixiang1988@gmail.com>
2015-01-15 15:57:53 +0800

    添加model

---
+++ WebApi2Book/src/WebApi2Book.Data/Entities/Status.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApi2Book.Data.Entities
+{
+    public class Status
+    {
+        public virtual long StatusId { get; set; }
+
+        public virtual string Name { get; set; }
+
+        public virtual int Ordinal { get; set; }
+
+        public virtual byte[] Version { get; set; }
+    }
+}
