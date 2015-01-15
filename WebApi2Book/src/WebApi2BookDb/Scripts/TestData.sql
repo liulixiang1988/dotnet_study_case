688bdac0d5678497cc62c46b1bec1ab78caaa7ab
liulixiang1988 <liulixiang1988@gmail.com>
2015-01-15 17:21:40 +0800

    添加数据库项目

---
+++ WebApi2Book/src/WebApi2BookDb/Scripts/TestData.sql
@@ -0,0 +1,69 @@
+﻿DECLARE @statusId INT ,
+    @taskId INT ,
+    @userId INT
+IF NOT EXISTS ( SELECT  *
+                FROM    [User]
+                WHERE   Username = 'bhogg' )
+    INSERT  INTO [dbo].[User]
+            ( [Firstname] ,
+              [Lastname] ,
+              [Username]
+            )
+    VALUES  ( N'Boss' ,
+              N'Hogg' ,
+              N'bhogg'
+            )
+IF NOT EXISTS ( SELECT  *
+                FROM    [User]
+                WHERE   Username = 'jbob' )
+    INSERT  INTO [dbo].[User]
+            ( [Firstname] ,
+              [Lastname] ,
+              [Username]
+            )
+    VALUES  ( N'Jim' ,
+              N'Bob' ,
+              N'jbob'
+            )
+IF NOT EXISTS ( SELECT  *
+                FROM    [User]
+                WHERE   Username = 'jdoe' )
+    INSERT  INTO [dbo].[User]
+            ( [Firstname] ,
+              [Lastname] ,
+              [Username]
+            )
+    VALUES  ( N'John' ,
+              N'Doe' ,
+              N'jdoe'
+            )
+IF NOT EXISTS ( SELECT  *
+                FROM    dbo.Task
+                WHERE   Subject = 'Test Task' )
+    BEGIN
+        SELECT TOP 1
+                @statusId = StatusId
+        FROM    Status
+        ORDER BY StatusId;
+        SELECT TOP 1
+                @userId = UserId
+        FROM    [User]
+        ORDER BY UserId;
+        INSERT  INTO dbo.Task
+                ( Subject ,
+                  StartDate ,
+                  StatusId ,
+                  CreatedDate ,
+                  CreatedUserId
+                )
+        VALUES  ( 'Test Task' ,
+                  GETDATE() ,
+                  @statusId ,
+                  GETDATE() ,
+                  @userId
+                );
+        SET @taskId = SCOPE_IDENTITY();
+        INSERT  [dbo].[TaskUser]
+                ( [TaskId], [UserId] )
+        VALUES  ( @taskId, @userId )
+    END
---
+++ WebApi2Book/src/WebApi2BookDb/Tables/TaskUser.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[TaskUser]
+    (
+      [TaskId] BIGINT NOT NULL ,
+      [UserId] BIGINT NOT NULL ,
+      [ts] rowversion NOT NULL ,
+      PRIMARY KEY ( TaskId, UserId ) ,
+      FOREIGN KEY ( UserId ) REFERENCES dbo.[User] ( UserId ) ,
+      FOREIGN KEY ( TaskId ) REFERENCES dbo.Task ( TaskId )
+    )
+go
+CREATE INDEX ix_TaskUser_UserId ON TaskUser(UserId)
+go
