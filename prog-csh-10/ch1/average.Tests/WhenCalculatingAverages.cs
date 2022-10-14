f860386069452ee352dab97910a020388fa17faa
Lixiang_Liu <lixiangliu@microsoft.com>
2022-10-14 22:14:01 +0800

    add tests

---
+++ prog-csh-10/ch1/average.Tests/WhenCalculatingAverages.cs
@@ -0,0 +1,21 @@
+namespace average.Tests;
+
+[TestClass]
+public class WhenCalculatingAverages
+{
+    [TestMethod]
+    public void SingleInputShouldProduceSameValueAsResult()
+    {
+        string[] inputs = { "1" };
+        double result = AverageCalculator.ArithmeticMean(inputs);
+        Assert.AreEqual(1.0, result, 1E-14);
+    }
+
+    [TestMethod]
+    public void MultipleInputsShouldProduceAverageAsResult()
+    {
+        string[] inputs = { "1", "2", "3" };
+        double result = AverageCalculator.ArithmeticMean(inputs);
+        Assert.AreEqual(2.0, result, 1E-14);
+    }
+}
