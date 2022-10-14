91aeca5ba5a8a9a9fddbeae3970c0e336b676837
Lixiang_Liu <lixiangliu@microsoft.com>
2022-10-14 22:56:25 +0800

    add implementation

--- prog-csh-10/ch1/averages/AverageCalculator.cs
+++ prog-csh-10/ch1/averages/AverageCalculator.cs
@@ -4,6 +4,7 @@ public static class AverageCalculator
 {
     public static double ArithmeticMean(string[] args)
     {
-        return 1.0;
+        return args.Select(numText => double.Parse(numText))
+                   .Average();
     }
 }
--- prog-csh-10/ch1/averages/Program.cs
+++ prog-csh-10/ch1/averages/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Averages;
+Console.WriteLine(AverageCalculator.ArithmeticMean(args));
