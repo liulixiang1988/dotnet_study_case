83338a57b66f06b7d15d257fd5159458b415d86d
Lixiang Liu <liulixiang1988@gmail.com>
2022-10-29 21:25:13 +0800

    switch pattern

---
+++ prog-csh-10/ch2/Pattern/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// read integer from console
+var value = int.Parse(Console.ReadLine() ?? String.Empty);
+
+switch (value)
+{
+    case < 0:
+        Console.WriteLine("Value is positive");
+        break;
+    case > 0:
+        Console.WriteLine("Value is negative");
+        break;
+    default:
+        Console.Write("Value is zero");
+        break;
+}
