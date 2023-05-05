224b5feaff11f5d8a4618dc74a2f6b32b98ad948
Lixiang Liu <liulixiang1988@gmail.com>
2023-05-05 09:47:42 +0800

    Update demo

--- aspnet-test/aspnet-test/aspnet-demo/Filters/BillFilter.cs
+++ aspnet-test/aspnet-test/aspnet-demo/Filters/BillFilter.cs
@@ -14,21 +14,28 @@ public class BillFilter : IActionFilter, IAsyncActionFilter, IAlwaysRunResultFil
         _logger = logger;
     }
 
+    public static string GetHttpMethod(RouteData routeData)
+    {
+        var controllerName = routeData.Values["controller"];
+        var actionName = routeData.Values["action"];
+        return $"{controllerName}#{actionName}";
+    }
+
     public void OnActionExecuting(ActionExecutingContext context)
     {
-        _logger.LogInformation("OnActionExecuting");
+        _logger.LogInformation("OnActionExecuting, {}", GetHttpMethod(context.RouteData));
     }
 
     public void OnActionExecuted(ActionExecutedContext context)
     {
-        _logger.LogInformation("OnActionExecuted, {context}", context);
+        _logger.LogInformation("OnActionExecuted, {} {context}", GetHttpMethod(context.RouteData), context);
     }
 
     public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
     {
         if (Configs.LocalValue.Value == null || !Configs.LocalValue.Value.StartsWith("From"))
             Configs.LocalValue.Value = "From OnActionExecutionAsync";
-        _logger.LogInformation("OnActionExecutionAsync, {context}", context);
+        _logger.LogInformation("OnActionExecutionAsync start, {} {context}", GetHttpMethod(context.RouteData), context);
         try
         {
             await next();
@@ -38,24 +45,25 @@ public class BillFilter : IActionFilter, IAsyncActionFilter, IAlwaysRunResultFil
             //q: object to json
             var json = JsonSerializer.Serialize(context.HttpContext.Request.Path);
             _logger.LogInformation(
-                "OnActionExecutionAsync, local {local}, http status code: {statusCode}, context {context}",
-                Configs.LocalValue.Value,
+                "OnActionExecutionAsync, {}, local {local}, http status code: {statusCode}, context {context}",
+                GetHttpMethod(context.RouteData), Configs.LocalValue.Value,
                 context.HttpContext.Response.StatusCode, json);
         }
     }
 
     public void OnResultExecuting(ResultExecutingContext context)
     {
+        _logger.LogInformation("OnResultExecuting start, {} {context}", GetHttpMethod(context.RouteData), context);
     }
 
     public void OnResultExecuted(ResultExecutedContext context)
     {
-        _logger.LogInformation("OnResultExecuted, {context}", context);
+        _logger.LogInformation("OnResultExecuted start, {} {context}", GetHttpMethod(context.RouteData), context);
     }
 
     public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
     {
-        _logger.LogInformation("OnResultExecutionAsync, {context}", context);
+        _logger.LogInformation("OnResultExecutionAsync {}, {context}", GetHttpMethod(context.RouteData), context);
         try
         {
             await next();
@@ -64,7 +72,8 @@ public class BillFilter : IActionFilter, IAsyncActionFilter, IAlwaysRunResultFil
         {
             var json = JsonSerializer.Serialize(context.HttpContext.Request.Path);
             _logger.LogInformation(
-                "OnResultExecutionAsync, local {}, http status code: {statusCode}, context {context}",
+                "OnResultExecutionAsync, {}, local {}, http status code: {statusCode}, context {context}",
+                GetHttpMethod(context.RouteData),
                 Configs.LocalValue.Value,
                 context.HttpContext.Response.StatusCode, json);
         }
@@ -72,12 +81,12 @@ public class BillFilter : IActionFilter, IAsyncActionFilter, IAlwaysRunResultFil
 
     public void OnResourceExecuting(ResourceExecutingContext context)
     {
-        _logger.LogInformation("OnResourceExecuting, {context}", context);
+        _logger.LogInformation("OnResourceExecuting, {}, {context}", GetHttpMethod(context.RouteData), context);
     }
 
     public void OnResourceExecuted(ResourceExecutedContext context)
     {
-        _logger.LogInformation("OnResourceExecuted, {context}", context);
+        _logger.LogInformation("OnResourceExecuted, {}, {context}", GetHttpMethod(context.RouteData), context);
     }
 
     public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
@@ -85,13 +94,15 @@ public class BillFilter : IActionFilter, IAsyncActionFilter, IAlwaysRunResultFil
         if (Configs.LocalValue.Value == null || !Configs.LocalValue.Value.StartsWith("From"))
         {
             context.HttpContext.Request.Headers.TryGetValue("x-local", out var local);
-            
-            Configs.LocalValue.Value = local.Count > 0 ? local.ToString() : "From OnResourceExecutionAsync" + System.DateTime.Now.Ticks;
+
+            Configs.LocalValue.Value = local.Count > 0
+                ? local.ToString()
+                : "From OnResourceExecutionAsync" + System.DateTime.Now.Ticks;
             context.HttpContext.Request.Headers["x-local"] = Configs.LocalValue.Value;
             context.HttpContext.Response.Headers["x-local"] = Configs.LocalValue.Value;
         }
 
-        _logger.LogInformation("OnResourceExecutionAsync, {context}", context);
+        _logger.LogInformation("OnResourceExecutionAsync,{}, {context}", GetHttpMethod(context.RouteData), context);
         try
         {
             await next();
@@ -100,7 +111,8 @@ public class BillFilter : IActionFilter, IAsyncActionFilter, IAlwaysRunResultFil
         {
             var json = JsonSerializer.Serialize(context.HttpContext.Request.Path);
             _logger.LogInformation(
-                "OnResourceExecutionAsync, local {}, http status code: {statusCode}, context {context}",
+                "OnResourceExecutionAsync, {}, local {}, http status code: {statusCode}, context {context}",
+                GetHttpMethod(context.RouteData),
                 Configs.LocalValue.Value,
                 context.HttpContext.Response.StatusCode, json);
         }
