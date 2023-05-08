bd1e10c62ce7ca48c38eb06642f2eba6e9819d51
Lixiang Liu <liulixiang1988@gmail.com>
2023-05-08 18:53:59 +0800

    Update

--- csharp_advance/python-hybrid/Program.cs
+++ csharp_advance/python-hybrid/Program.cs
@@ -23,22 +23,22 @@ using (Py.GIL())
     var b64 = base64.b64encode(img.tobytes());
     Console.WriteLine(b64);
     Console.WriteLine("\n\n------\n\n");
-    dynamic np = Py.Import("numpy");
-    Console.WriteLine(np.cos(np.pi * 2));
-
-    dynamic sin = np.sin;
-    Console.WriteLine(sin(5));
-
-    double c = (double)(np.cos(5) + sin(5));
-    Console.WriteLine(c);
-
-    dynamic a = np.array(new List<float> { 1, 2, 3 });
-    Console.WriteLine(a.dtype);
-
-    dynamic b = np.array(new List<float> { 6, 5, 4 }, dtype: np.int32);
-    Console.WriteLine(b.dtype);
-
-    Console.WriteLine(a * b);
+    
+    // dynamic np = Py.Import("numpy");
+    // Console.WriteLine(np.cos(np.pi * 2));
+    // dynamic sin = np.sin;
+    // Console.WriteLine(sin(5));
+    //
+    // double c = (double)(np.cos(5) + sin(5));
+    // Console.WriteLine(c);
+    //
+    // dynamic a = np.array(new List<float> { 1, 2, 3 });
+    // Console.WriteLine(a.dtype);
+    //
+    // dynamic b = np.array(new List<float> { 6, 5, 4 }, dtype: np.int32);
+    // Console.WriteLine(b.dtype);
+    //
+    // Console.WriteLine(a * b);
 }
 
 Console.WriteLine("exit");
